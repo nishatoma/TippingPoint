0c3b1b061a4fc55a11cffeead5b11d648b80eeee
nishatoma <nishatoma@gmail.com>
2021-12-14 12:42:17 -0500

    Add first TopHeader

--- app/src/main/java/com/example/tippingpoint/MainActivity.kt
+++ app/src/main/java/com/example/tippingpoint/MainActivity.kt
@@ -3,11 +3,22 @@ package com.example.tippingpoint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tippingpoint.ui.theme.TippingPointTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +26,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TippingPointTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                MyApp {
+                    TopHeader()
                 }
             }
         }
     }
 }
 
+// Container functions
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    // A surface container using the 'background' color from the theme
+    Surface(color = MaterialTheme.colors.background) {
+        content()
+    }
 }
+
+
+@Composable
+fun TopHeader() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        elevation = 4.dp,
+        color = Color.Magenta
+    ) {
+        Column {
+            Text(text = "Total Tip:")
+            Text(text = "$100")
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TippingPointTheme {
-        Greeting("Android")
+        MyApp {
+            Text(text = "Hello Again!")
+        }
     }
 }
