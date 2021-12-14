b729f455bc75b836565fe43952a3b67d1bc07a25
nishatoma <nishatoma@gmail.com>
2021-12-14 12:55:42 -0500

    Finish TopHeader

--- app/src/main/java/com/example/tippingpoint/MainActivity.kt
+++ app/src/main/java/com/example/tippingpoint/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.tippingpoint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,9 +11,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tippingpoint.ui.theme.TippingPointTheme
@@ -43,20 +44,33 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
-
+@Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 0.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
-            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+            .clip(shape = RoundedCornerShape(corner = CornerSize(40.dp)))
+            .padding(10.dp),
         elevation = 4.dp,
-        color = Color.Magenta
+        color = Color(0xFFE9D7F7)
     ) {
-        Column {
-            Text(text = "Total Tip:")
-            Text(text = "$100")
+        Column(
+            modifier = Modifier.padding(12.dp), horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total per person",
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
